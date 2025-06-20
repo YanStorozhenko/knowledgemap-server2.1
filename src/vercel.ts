55bd5fe975dd3fd92ab5b7288d001525237b8a78
YanStorozhenko <c.storozhenko.yan@student.uzhnu.edu.ua>
2025-06-20 03:43:27 +0300

    dep2 fix

--- src/vercel.ts
+++ src/vercel.ts
@@ -1,13 +1,18 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const server = express();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
     await app.init();
+
+    // (опціонально) відповісти одразу на / поки Nest не готовий
+    server.get('/', (_req: Request, res: Response) => {
+        res.send('Hello from NestJS on Vercel!');
+    });
 }
 
 bootstrap();

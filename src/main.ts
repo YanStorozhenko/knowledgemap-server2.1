ca657db6f31a822a353907d0f8a758652b296381
YanStorozhenko <c.storozhenko.yan@student.uzhnu.edu.ua>
2025-06-20 21:55:00 +0300

    fix Back main.ts app.use

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,20 @@ async function bootstrap() {
         credentials: true,
     });
 
+//////////////
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        res.header('Access-Control-Allow-Credentials', 'true');
 
+        if (req.method === 'OPTIONS') {
+            return res.sendStatus(200);
+        }
+
+        next();
+    });
+//////////////
 
 
 

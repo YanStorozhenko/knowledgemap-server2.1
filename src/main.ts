9788f57353d2bba705b5ae5cd4a98bb889dbfe94
YanStorozhenko <c.storozhenko.yan@student.uzhnu.edu.ua>
2025-06-20 22:28:56 +0300

    app use

--- src/main.ts
+++ src/main.ts
@@ -33,18 +33,18 @@ async function bootstrap() {
     // });
 
 //////////////
-//     app.use((req, res, next) => {
-//         res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-//         res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-//         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//         res.header('Access-Control-Allow-Credentials', 'true');
-//
-//         if (req.method === 'OPTIONS') {
-//             return res.sendStatus(200);
-//         }
-//
-//         next();
-//     });
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        res.header('Access-Control-Allow-Credentials', 'true');
+
+        if (req.method === 'OPTIONS') {
+            return res.sendStatus(200);
+        }
+
+        next();
+    });
 //////////////
 
 

07457aa50dc679313802d5ec8b4ffa0dbb260b99
YanStorozhenko <c.storozhenko.yan@student.uzhnu.edu.ua>
2025-06-20 20:55:49 +0300

    fix 11

--- src/vercel.ts
+++ src/vercel.ts
@@ -1,26 +1,49 @@
+// import { AppModule } from './app.module';
+// import { NestFactory } from '@nestjs/core';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import express from 'express';
+//
+// const server = express();
+//
+// async function bootstrap() {
+//     const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+//     app.setGlobalPrefix('api');
+//     await app.init();
+//
+//     // Hello на /
+//     server.get('/', (_req, res) => {
+//         res.send('Hello from NestJS on Vercel!');
+//     });
+//
+//     // Проксі для API
+//     server.all('/api/*', (req, res) => {
+//         app.getHttpAdapter().getInstance()(req, res);
+//     });
+// }
+//
+// bootstrap();
+//
+// export default server;
+
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const server = express();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-    app.setGlobalPrefix('api');
     await app.init();
 
-    // Hello на /
-    server.get('/', (_req, res) => {
+    // (опціонально) відповісти одразу на / поки Nest не готовий
+    server.get('/', (_req: Request, res: Response) => {
         res.send('Hello from NestJS on Vercel!');
-    });
-
-    // Проксі для API
-    server.all('/api/*', (req, res) => {
-        app.getHttpAdapter().getInstance()(req, res);
     });
 }
 
 bootstrap();
 
 export default server;
+
+

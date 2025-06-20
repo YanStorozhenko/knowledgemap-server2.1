623dac507da0ae46f2829fee11a9327bdc1c40c1
YanStorozhenko <c.storozhenko.yan@student.uzhnu.edu.ua>
2025-06-20 03:40:49 +0300

    dep2 fix

--- src/vercel.ts
+++ src/vercel.ts
@@ -1,28 +1,15 @@
-import { createServer, proxy } from 'aws-serverless-express';
-import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
-
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 
+const server = express();
 
-let cachedServer: any;
-
-async function bootstrapServer(): Promise<any> {
-    const expressApp = express();
-    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
     await app.init();
-    return createServer(expressApp);
 }
 
-export const handler: Handler = async (
-    event: any,
-    context: Context,
-    callback: Callback,
-) => {
-    if (!cachedServer) {
-        cachedServer = await bootstrapServer();
-    }
-    return proxy(cachedServer, event, context, 'PROMISE').promise;
-};
+bootstrap();
+
+export default server;
